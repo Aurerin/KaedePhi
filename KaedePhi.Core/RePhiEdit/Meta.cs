c560064d4bfb97e72e41b09d34ad0c2ed273407b
NuanRMxi <2308425927@qq.com>
2026-05-10 17:22:56 +0800

    fix: META转info.txt格式不正确

--- KaedePhi.Core/RePhiEdit/Meta.cs
+++ KaedePhi.Core/RePhiEdit/Meta.cs
@@ -1,6 +1,5 @@
-﻿using Newtonsoft.Json;
-
-// STJ 特性使用完全限定名
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
 
 namespace KaedePhi.Core.RePhiEdit
 {
@@ -12,59 +11,78 @@ namespace KaedePhi.Core.RePhiEdit
         /// <summary>
         /// RePhiEdit版本号
         /// </summary>
-        [JsonProperty("RPEVersion")] public int RpeVersion = 170; // RPE版本
+        [JsonProperty("RPEVersion")]
+        public int RpeVersion { get; set; } = 170; // RPE版本
 
         /// <summary>
         /// 曲绘的相对路径
         /// </summary>
-        [JsonProperty("background")] public string Background = "0.jpg"; // 曲绘
+        [JsonProperty("background")]
+        public string Background { get; set; } = "0.jpg"; // 曲绘
 
         /// <summary>
         /// 谱师
         /// </summary>
-        [JsonProperty("charter")] public string Charter = "PhiFanmadeCore"; // 谱师
+        [JsonProperty("charter")]
+        public string Charter { get; set; } = "KaedePhiCore"; // 谱师
 
         /// <summary>
         /// 谱面音乐作者
         /// </summary>
-        [JsonProperty("composer")] public string Composer = "Unknown"; // 曲师
+        [JsonProperty("composer")]
+        public string Composer { get; set; } = "Unknown"; // 曲师
 
         /// <summary>
         /// 谱面曲绘作者
         /// </summary>
-        [JsonProperty("illustration")] public string Illustration = "Unknown"; // 曲绘画师
+        [JsonProperty("illustration")]
+        public string Illustration { get; set; } = "Unknown"; // 曲绘画师
 
         /// <summary>
         /// 谱面难度
         /// </summary>
-        [JsonProperty("level")] public string Level = "NR  Lv.17"; // 难度
+        [JsonProperty("level")]
+        public string Level { get; set; } = "NR  Lv.17"; // 难度
 
         /// <summary>
         /// 谱面名称
         /// </summary>
-        [JsonProperty("name")] public string Name = "PhiFanmadeCore by NuanR_Star Ciallo Team"; // 曲名
+        [JsonProperty("name")]
+        public string Name { get; set; } = "KaedePhiCore by NuanR_Star Ciallo Team"; // 曲名
 
         /// <summary>
         /// 谱面偏移，单位为毫秒
         /// </summary>
-        [JsonProperty("offset")] public int Offset = 0; // 音乐偏移
+        [JsonProperty("offset")]
+        public int Offset { get; set; } = 0; // 音乐偏移
 
         /// <summary>
         /// 音乐的相对路径
         /// </summary>
-        [JsonProperty("song")] public string Song = "0.wav"; // 音乐
+        [JsonProperty("song")]
+        public string Song { get; set; } = "0.wav"; // 音乐
+
+        /// <summary>
+        /// 谱面唯一标识符
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; } = "0";
 
         public override string ToString()
+            => ToString(Id + ".json");
+
+        [PublicAPI]
+        public string ToString(string fileName)
         {
-            return $"RPEVersion: {RpeVersion}\n" +
-                   $"Background: {Background}\n" +
-                   $"Charter: {Charter}\n" +
-                   $"Composer: {Composer}\n" +
-                   $"Illustration: {Illustration}\n" +
-                   $"Level: {Level}\n" +
+            return "#\n" +
                    $"Name: {Name}\n" +
-                   $"Offset: {Offset}\n" +
-                   $"Song: {Song}\n";
+                   $"Path: {Id}\n" +
+                   $"Song: {Song}\n" +
+                   $"Picture: {Background}\n" +
+                   $"Chart: {fileName}\n" +
+                   $"Level: {Level}\n" +
+                   $"Composer: {Composer}\n" +
+                   $"Charter: {Charter}\n";
         }
 
         public Meta Clone()
