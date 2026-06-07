e048d2dcdede39b5e4d347bbe3794bd3fb808aa9
NuanRMxi <2308425927@qq.com>
2026-06-07 15:20:55 +0800

    feat: 支持PhiZonePlayer附加在RePhiEditJson中的字段，extra.json暂不考虑

---
+++ KaedePhi.Core/PhiZonePlayerExtended/JudgeLines/AppearanceOnAttach.cs
@@ -0,0 +1,21 @@
+﻿namespace KaedePhi.Core.PhiZonePlayerExtended.JudgeLines
+{
+    /// <summary>
+    /// 决定当UI组件或任何视频附加到该线上时，该线将如何显示。
+    /// </summary>
+    public enum AppearanceOnAttach
+    {
+        /// <summary>
+        /// 隐藏
+        /// </summary>
+        Hidden = 0,
+        /// <summary>
+        /// 白色
+        /// </summary>
+        WhiteColored = 1,
+        /// <summary>
+        /// FullCombo/AllPrefect指示器颜色
+        /// </summary>
+        FcApColor = 2,
+    }
+}
